8bf2dc333ffedb90c01f9039c62612b17987e7c3
Robert Desjardins <roberttdesjardins@gmail.com>
2018-05-02 17:01:56 -0700

    First commit, testing side scroller game

---
+++ Side Scroller/GameScene.swift
@@ -0,0 +1,54 @@
+//
+//  GameScene.swift
+//  Side Scroller
+//
+//  Created by Robert Desjardins on 2018-05-02.
+//  Copyright © 2018 Robert Desjardins. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    let worldNode = SKNode()
+    
+    var player = SKSpriteNode(color: UIColor.white, size: CGSize(width: 20, height: 20))
+    
+  
+    override func sceneDidLoad() {
+        addChild(worldNode)
+        physicsWorld.contactDelegate = self
+        setUpPlayer()
+    }
+    
+    func setUpPlayer() {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
---
+++ Side ScrollerTests/Side_ScrollerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Side_ScrollerTests.swift
+//  Side ScrollerTests
+//
+//  Created by Robert Desjardins on 2018-05-02.
+//  Copyright © 2018 Robert Desjardins. All rights reserved.
+//
+
+import XCTest
+@testable import Side_Scroller
+
+class Side_ScrollerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
